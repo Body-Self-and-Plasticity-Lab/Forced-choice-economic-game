ace223dc7beb75097b61fda7ed9da55cc8f531c7
Marte Roel <marteroel@users.noreply.github.com>
2018-08-29 11:54:48 +0200

    improved UI, colliders outside of screen, feedback buttons in screen

--- ForcedChoice interactions/Assets/Alternating choice/Scripts/TriggerInteractions.cs
+++ ForcedChoice interactions/Assets/Alternating choice/Scripts/TriggerInteractions.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿	using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,25 +9,31 @@ namespace AlternatingForcedChoice {
 			
 		public GameObject contentManager;
 
-		public GameObject selection1, selection2;
+		public GameObject selection1, selection2, goToNext;
 		public Material notSelected, selected;
+
+		public GameObject image1, image2, nextImage;
+
+		public bool useImage;
 
 		private CsvWrite writer;
 		private TaskContentIntegration textContent;
 
 		public static string forcedResponse;
 
-		public GameObject goToNext;
 		public Text questionText;
 
 		public static string responseAtext, responseBtext;
 		public static float responseTime;
 		private float timeAtStart;
 
+		private Color turnedOnColor;
+
 
 		void Start () {
 			
 			goToNext.SetActive (false);
+			if (nextImage != null)	nextImage.SetActive (false);
 			selection1.SetActive (false);
 			selection2.SetActive (false);
 
@@ -35,6 +41,8 @@ namespace AlternatingForcedChoice {
 
 			textContent = contentManager.GetComponent<TaskContentIntegration> ();
 			writer = contentManager.GetComponent<CsvWrite> ();
+
+			turnedOnColor = new Color(1f, 0.85f, 0.55f);
 		}
 
 		private IEnumerator SetupInteractions() {
@@ -54,26 +62,52 @@ namespace AlternatingForcedChoice {
 
 			if (other == selection1.GetComponent<Collider>()) {
 				forcedResponse = textContent.responseTextA;
-				selection1.GetComponent<Renderer> ().material = selected;
-				selection2.GetComponent<Renderer> ().material = notSelected;
+
+				if (!useImage) {	
+					selection1.GetComponent<Renderer> ().material = selected; 
+					selection2.GetComponent<Renderer> ().material = notSelected;
+				} 
+				else {
+					image1.GetComponent<Image> ().color = turnedOnColor;
+					image2.GetComponent<Image> ().color = Color.gray;
+				}
+
 				goToNext.SetActive (true);
+				if (nextImage != null)	nextImage.SetActive (true);
 			}
 
 			if (other == selection2.GetComponent<Collider>()) {
 				forcedResponse = textContent.responseTextB;
-				selection2.GetComponent<Renderer> ().material = selected;
-				selection1.GetComponent<Renderer> ().material = notSelected;
+
+				if (!useImage) {	
+					selection2.GetComponent<Renderer> ().material = selected;
+					selection1.GetComponent<Renderer> ().material = notSelected;
+				}
+
+				else {
+					image2.GetComponent<Image> ().color = turnedOnColor;
+					image1.GetComponent<Image> ().color = Color.gray;
+				}
+
 				goToNext.SetActive (true);
+				if (nextImage != null)	nextImage.SetActive (true);
 			}
 
 			if (other == goToNext.GetComponent<Collider>()) {
 
 				responseTime = Time.fixedTime - timeAtStart;
 				goToNext.SetActive (false);
+				if (nextImage != null)	nextImage.SetActive (false);
+			
+				if (!useImage) {
+					selection1.GetComponent<Renderer> ().material = notSelected;
+					selection2.GetComponent<Renderer> ().material = notSelected;
+				} 
 
-				selection1.GetComponent<Renderer> ().material = notSelected;
-				selection2.GetComponent<Renderer> ().material = notSelected;
-
+				else {
+					image2.GetComponent<Image> ().color = Color.gray;
+					image1.GetComponent<Image> ().color = Color.gray;
+				}
 				writer.onNextButtonPressed ();
 				textContent.OnNextButton ();
 
